aa8a9c83c59c6e703470417608883b2d52f2b662
Jarne <jarne@van-der-plas.net>
2022-04-17 01:15:45 +0200

    Working communication with x, y , heading and id

--- Bachelorproef/ArucoRecog/basic_serial/basic_serial.ino
+++ Bachelorproef/ArucoRecog/basic_serial/basic_serial.ino
@@ -1,10 +1,26 @@
 void setup() {
   Serial.begin(9600);
 }
+
+int x_ard, y_ard, heading_ard, id_ard;
+
 void loop() {
   if (Serial.available() > 0) {
     String data = Serial.readStringUntil('\n');
+    int str_len = data.length() + 1;
+    char char_array[str_len];
+    data.toCharArray(char_array, str_len);
+    sscanf(data.c_str(), "%d;%d;%d;%d", &id_ard, &x_ard, &y_ard, &heading_ard);
+    
     Serial.print("You sent me: ");
-    Serial.println(data);
+    Serial.print(data);
+    Serial.print(" : ");
+    Serial.print(id_ard);
+    Serial.print(" & ");
+    Serial.print(x_ard);
+    Serial.print(" & ");
+    Serial.print(y_ard);
+    Serial.print(" & ");
+    Serial.println(heading_ard);
   }
 }
