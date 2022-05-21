9e6a3f6e911ec5161ddaac22df031392960391cd
Jarne <83751897+JarneVdP@users.noreply.github.com>
2022-05-21 22:09:05 +0200

    Update sendrec.py

--- Bachelorproef/TestAR/sendrec.py
+++ Bachelorproef/TestAR/sendrec.py
@@ -2,13 +2,19 @@
 import serial
 import time
 
+emptyserial = -99
+idTag =  1
+x = 2
+y = 3
+heading = 4
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
     starttime = time.time()
     endtime = time.time()
     while endtime - starttime < 3:
-        ser.write("Hello from Raspberry Pi!\n".encode('utf-8'))
+        ser.write(("%d;%d;%d;%d;%d\n" % (emptyserial ,idTag,x, y, heading)).encode('utf-8'))
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
@@ -16,3 +22,4 @@ if __name__ == '__main__':
         #time.sleep(60.0 - ((time.time() - starttime) % 60.0))
     print("done")
     
+
